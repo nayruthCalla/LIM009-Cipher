6b2479d2c5906f8ef522fe29d6c144e245398af1
nayruthCalla <nayruth.calla@gmail.com>
2019-02-20 02:33:24 -0500

    bienvenida en el index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 /* Acá va tu código */
+
+function showname() {
+  const userName = document.getElementById("user").value;
+  if (userName.length == 0) {
+    alert("INGRESE SU NOMBRE POR FAVOR :)");
+  } else {
+    document.getElementById("name").innerHTML = "Bienvenida  " + userName;
+    root.style.display = "none";
+
+    document.getElementById("welcome").style.display = "block";
+  }
+}
