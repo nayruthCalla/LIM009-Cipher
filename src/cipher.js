971b50759ec6d6c6932ec5271b34b5444d3b5322
nayruthCalla <nayruth.calla@gmail.com>
2019-02-24 22:07:42 -0500

    Parte obligatoria resuelta

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,34 @@
 window.cipher = {
-  encode: () => {
+  encode: (offset, string) => {
+    //declarando parámetros al método encode
+    arrConvr = [];
+    let getAscii = 0;
     /* Acá va tu código */
+
+    for (let i = 0; i < string.length; i++) {
+      getAscii = string.charCodeAt(i); //obteniendo el valor numérico de codigo Ascii
+      if (65 <= getAscii && getAscii <= 90) {
+        // Validamos si las letras ingresadas son mayúsculas
+        arrConvr += String.fromCharCode(((getAscii - 65 + offset) % 26) + 65); // concatenando cada letra para formar una palabra
+      } else {
+        alert("ingrese una letra mayúscula xf");
+      }
+    }
+    return arrConvr;
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (offset, string) => {
+    arrDecode = [];
+    //declarando parámetros al método encode
+    /* Acá vvvva tu código */
+    for (let i = 0; i < string.length; i++) {
+      getAscii = string.charCodeAt(i); //obteniendo el valor numerico de codigo Ascii
+      if (65 <= getAscii && getAscii <= 90) {
+        // Validamos si las letras ingresadas son mayúsculas
+        arrDecode += String.fromCharCode(((getAscii - 90 - offset) % 26) + 90); // concatenando cada letra para formar una palabra
+      } else {
+        alert("ingrese mayúsculas por favor :)");
+      }
+    }
+    return arrDecode;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 /* Acá va tu código */
 
-function showname() {
+const showname = () => {
   const userName = document.getElementById("user").value;
   if (userName.length == 0) {
     alert("INGRESE SU NOMBRE POR FAVOR :)");
   } else {
-    document.getElementById("name").innerHTML = "Bienvenida  " + userName;
-    root.style.display = "none";
+    document.getElementById(
+      "welcomeUser"
+    ).innerHTML = ` BIENVENIDA ${userName}`;
 
-    document.getElementById("welcome").style.display = "block";
+    document.getElementById("root").style.display = "none";
+    document.getElementById("form-decode").style.display = "block";
   }
-}
+};
+
+const btnEncod = () => {
+  let getOffset = parseInt(document.getElementById("offset").value);
+  let getString = String(document.getElementById("password").value);
+  document.getElementById("setPass").innerHTML = cipher.encode(
+    getOffset,
+    getString
+  );
+};
+
+const btnDecod = () => {
+  let getOffset = parseInt(document.getElementById("offset").value);
+  let getString = String(document.getElementById("password").value);
+  document.getElementById("setPass").innerHTML = cipher.decode(
+    getOffset,
+    getString
+  );
+};
+
+const btnClear = () => {
+  document.getElementById("offset").value = "";
+  document.getElementById("password").value = "";
+};
